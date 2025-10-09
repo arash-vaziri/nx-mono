63039e342332bf8cd1e0e63aa90e3fa171f7ffb2
arash-vaziri <arash.h.vaziri@gmail.com>
2025-10-09 16:12:43 -0400

    - Intro Structure

---
+++ api/src/app/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+
+    @Get()
+    findAll() {
+        return [];
+    }
+
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+
+        return {
+            id
+        }
+    }
+
+    @Post()
+    create(@Body() user:{}) {
+
+        return user;
+
+    }
+
+
+
+
+}
